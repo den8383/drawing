97f6ddd10283fb07dc8a9225af7115a44ac6f7fd
den8383 <den8383arfp@gmail.com>
2021-07-12 16:08:53 +0900

    [add] RootLink and DrawingLink

--- front/app/pages/index.tsx
+++ front/app/pages/index.tsx
@@ -1,5 +1,9 @@
+import Link from 'next/link'
 export const Home = (): JSX.Element => (
+    <div>
     <p>root page</p>
+    <Link href='/posts/drawing'>drawing</Link>
+    </div>
 )
 
 export default Home
