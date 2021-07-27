b428eda7b3816d8693257f99d5433d9402a0e072
den8383 <den8383arfp@gmail.com>
2021-07-27 16:52:07 +0900

    [fix] manage color value in drawing

--- front/app/pages/posts/drawing.tsx
+++ front/app/pages/posts/drawing.tsx
@@ -1,16 +1,26 @@
 import Link from 'next/link'
+import {useState} from 'react'
 import CanvasPanel from '../../components/CanvasPanel'
 import PalettePanel from '../../components/PalettePanel'
 import ToolsPanel from '../../components/ToolsPanel'
 
-export const Drawing = (): JSX.Element => (
+export const Drawing = () =>{
+  const [color,setColor] = useState("black")
+
+  const colorButtonClick = (color) =>{
+    setColor(color)
+  }
+
+  return (
     <div>
-    <CanvasPanel x='0' y='0'></CanvasPanel>
-    <PalettePanel></PalettePanel>
-    <ToolsPanel></ToolsPanel>
-    <p>drawing</p>
-    <Link href='/'>root</Link>
+      <CanvasPanel x='0' y='0'></CanvasPanel>
+      <PalettePanel colorButtonClick={colorButtonClick} color={color}></PalettePanel>
+      <ToolsPanel></ToolsPanel>
+      <p>drawing</p>
+      <Link href='/'>root</Link>
     </div>
-)
+  )
+}
+
 
 export default Drawing
